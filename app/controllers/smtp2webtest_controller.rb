33d5c27f0eb0d4dc5b69d9465fd5e8b5b8c30b26
Santhosh <santhoshonet@gmail.com>
2011-03-30 11:45:19 +0530

    added route mapping for /index url

--- app/controllers/smtp2webtest_controller.rb
+++ app/controllers/smtp2webtest_controller.rb
@@ -2,15 +2,15 @@ class Smtp2webtestController < ApplicationController
 
   def index
 
+    unless params[:body].nil?
       mstorage = Mailstorage.new
       mstorage.data = params[:body]
       mstorage.save
+    end
 
   end
 
   def view
-
-
 
   end
 
--- config/routes.rb
+++ config/routes.rb
@@ -51,6 +51,7 @@ Smtp2webtestapp::Application.routes.draw do
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
    root :controller => "smtp2webtest" , :action => "view"
+   match 'index', :controller => "smtp2webtest", :action => "index"
 
   # See how all your routes lay out with "rake routes"
 
